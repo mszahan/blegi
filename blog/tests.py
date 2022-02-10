d383e4276d43a4e7c850d852e744ced6883dccb9
mszahan <mszahan1@gmail.com>
2022-02-10 22:32:13 +0600

    started new project for blegi finished the 5th chapter
    started new app named blog
    added initial setup

---
+++ blog/tests.py
@@ -0,0 +1,46 @@
+from urllib import response
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from .models import Post
+
+
+
+class BlogTsts(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username = 'testuser',
+            email = 'test@email.com',
+            password = 'secret'
+        )
+
+        self.post = Post.objects.create(
+            title = 'A good testing title',
+            body = 'What am I writing here?',
+            author = self.user
+        )
+    
+
+    def test_string_representation(self):
+        post = Post(title='A good testing title'),
+        self.assertEqual(str(post), post.title)
+    
+    def test_post_content(self):
+        self.assertEqual(f"{self.post.title}", 'A good testing title')
+        self.assertEqual(f"{self.post.author}", 'testuser')
+        self.assertEqual(f"{self.post.body}", 'What am I writing here?')
+    
+    def test_post_list(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'What am I writing here?')
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_post_detail_view(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good testing title')
+        self.assertTemplateUsed(response, 'post_detail.html')
---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from pyexpat import model
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render
+from .models import Post
+
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
