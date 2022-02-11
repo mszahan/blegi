eaf152376eb744a299003ec8d22420076dbc4375
mszahan <mszahan1@gmail.com>
2022-02-11 10:25:14 +0600

    Just added very basic user autherntication design is ugly just the functionality

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    # these two url order matters
+    path('accounts/',include('accounts.urls')),
     path('', include('blog.urls')),
 ]
